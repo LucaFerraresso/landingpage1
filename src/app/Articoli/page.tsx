61f720a30b039815dcf89d1266632520c0062cf8
lucaferraresso <lucaferraresso96@gmail.com>
2025-01-23 11:53:29 +0100

    add array data/articoli.ts

--- src/app/Articoli/page.tsx
+++ src/app/Articoli/page.tsx
@@ -7,60 +7,12 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"; // Assicurati che questi componenti siano correttamente importati
+import { articoli } from "../../../data/Articoli";
 
 import { useState } from "react";
 import Link from "next/link"; // Importa il componente Link
 
 const ArticoliPage = () => {
-  // Array degli articoli direttamente all'interno del file
-  const Articoli = [
-    {
-      id: 1,
-      titolo: "Articolo 1",
-      descrizione: "Descrizione dell'articolo 1",
-      immagine: "/images/articolo1.jpg",
-      tipologia: "Tecnologia",
-      materiale: "Metallo",
-      prezzo: "199.99",
-    },
-    {
-      id: 2,
-      titolo: "Articolo 2",
-      descrizione: "Descrizione dell'articolo 2",
-      immagine: "/images/articolo2.jpg",
-      tipologia: "Elettronica",
-      materiale: "Plastica",
-      prezzo: "89.99",
-    },
-    {
-      id: 3,
-      titolo: "Articolo 3",
-      descrizione: "Descrizione dell'articolo 3",
-      immagine: "/images/articolo3.jpg",
-      tipologia: "Giocattolo",
-      materiale: "Legno",
-      prezzo: "49.99",
-    },
-    {
-      id: 4,
-      titolo: "Articolo 4",
-      descrizione: "Descrizione dell'articolo 4",
-      immagine: "/images/articolo4.jpg",
-      tipologia: "Arredamento",
-      materiale: "Legno",
-      prezzo: "149.99",
-    },
-    {
-      id: 5,
-      titolo: "Articolo 5",
-      descrizione: "Descrizione dell'articolo 5",
-      immagine: "/images/articolo5.jpg",
-      tipologia: "Sport",
-      materiale: "Acciaio",
-      prezzo: "99.99",
-    },
-  ];
-
   return (
     <div className="min-h-screen bg-gradient-to-r from-blue-50 to-blue-100 text-gray-800 dark:text-gray-200 dark:from-gray-800 dark:to-gray-900 py-8">
       <div className="container mx-auto px-4">
@@ -73,7 +25,7 @@ const ArticoliPage = () => {
         {/* Carousel */}
         <Carousel>
           <CarouselContent>
-            {Articoli.map((articolo) => (
+            {articoli.map((articolo) => (
               <CarouselItem key={articolo.id}>
                 <div className="p-6 bg-white dark:bg-gray-800 rounded shadow-md">
                   <img
